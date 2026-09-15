81166f7eaddfe182472db13e26dd7e0496e8f6cf
Emmanuel Echeverry <emmanuelecheverrybarrera6@gmail.com>
2026-09-15 13:23:22 -0500

    Problema4 solucionado

--- problema4/main.go
+++ problema4/main.go
@@ -29,6 +29,7 @@ func consumidor(id int, in <-chan int, wg *sync.WaitGroup) {
 		fmt.Printf("[consumidor %d] recibe %d\n", id, v)
 		// TODO: simular trabajo
 		// usa Sleep con un valor aleatorio entre 100 y 500 ms
+		time.Sleep(time.Duration(rand.Intn(400)+100) * time.Millisecond)
 	}
 	fmt.Printf("[consumidor %d] canal cerrado, termina\n", id)
 }
@@ -38,12 +39,13 @@ func main() {
 	valores := 10
 	consumidores := 3
 
-	ch := make(chan int) // TODO: prueba cambiar a canal bufferizado: make(chan int, 4)
+	ch := make(chan int, 2) // TODO: prueba cambiar a canal bufferizado: make(chan int, 4)
 
 	var wg sync.WaitGroup
 	wg.Add(consumidores)
 	// TODO: lanzar las goroutines consumidoras
 	for i := 1; i <= consumidores; i++ {
+		go consumidor(i, ch, &wg)
 
 	}
 
